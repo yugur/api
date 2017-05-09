caaf4ca99c44a56e726012d73f6cb926a328126a
Nicholas Brown <nicholasbrown@users.noreply.github.com>
2017-05-09 10:43:11 +1000

    Improved DB scripts
    - Update scripts/demo.sql to more closely match new design
    - Add scripts/drop.sql for faster testing.

--- scripts/demo.sql
+++ scripts/demo.sql
@@ -1,33 +1,50 @@
 CREATE TABLE languages (
-	id			bigserial	PRIMARY KEY,
+	lang_id		bigserial	PRIMARY KEY,
 	name		text		NOT NULL,
 	code		text		NOT NULL UNIQUE
 );
 
 CREATE TABLE wordtypes (
-	id			bigserial	PRIMARY KEY,
+	wordtype_id	bigserial	PRIMARY KEY,
+	name		text		NOT NULL UNIQUE
+);
+
+CREATE TABLE tags (
+	tag_id		bigserial		PRIMARY KEY,
 	name		text		NOT NULL UNIQUE
 );
 
 CREATE TABLE entries (
-	id			bigserial 	PRIMARY KEY,
+	entry_id		bigserial 	PRIMARY KEY,
 	headword	text 		NOT NULL,
-	type		bigint		REFERENCES wordtypes(id) ON DELETE CASCADE,
+	wordtype	bigint		REFERENCES wordtypes (wordtype_id),
 	definition	text		,
-	tags		text[]		,
-	language	bigint		REFERENCES languages(id) ON DELETE CASCADE
+	hw_lang		bigint		REFERENCES languages (lang_id) ON DELETE CASCADE,
+	def_lang	bigint		REFERENCES languages (lang_id) ON DELETE CASCADE
+);
+
+CREATE TABLE entry_tags (
+	tag_id		bigint,
+	entry_id	bigint,
+	CONSTRAINT PK_entry_tags PRIMARY KEY
+    (
+        tag_id,
+        entry_id
+    ),
+    FOREIGN KEY (tag_id) REFERENCES tags (tag_id),
+    FOREIGN KEY (entry_id) REFERENCES entries (entry_id)
 );
 
 CREATE TABLE users (
-	id			bigserial	PRIMARY KEY,
+	user_id		bigserial	PRIMARY KEY,
 	username	text		NOT NULL UNIQUE,
 	hash		text		NOT NULL,
 	email		text		NOT NULL UNIQUE,
 	dob			date		,
 	gender		text		,
 	joindate	timestamp	NOT NULL,
-	language	bigint		REFERENCES languages(id) ON DELETE CASCADE,
-	fluency		int[]
+	language	bigint		REFERENCES languages (lang_id),
+	fluency		int[]		
 );
 
 INSERT INTO languages (name, code) VALUES
@@ -42,6 +59,38 @@ INSERT INTO wordtypes (name) VALUES
 	('verb'),
 	('adjective');
 
-INSERT INTO entries (headword, type, definition, tags, language) VALUES
-	('fire', (SELECT id FROM languages WHERE name='noun'), 'Burning fuel or other material: a cooking fire; a forest fire.', '{"flame", "blaze"}', (SELECT id FROM languages WHERE code='en-AU')),
-	('fire', (SELECT id FROM languages WHERE name='noun'), 'Burning intensity of feeling; ardor.', '{"passion", "devotion"}', (SELECT id FROM languages WHERE code='en-AU'));
+INSERT INTO tags (name) VALUES
+	('flame'),
+	('blaze'),
+	('passion'),
+	('fervor');
+
+INSERT INTO entries (headword, wordtype, definition, hw_lang, def_lang) VALUES
+	('fire', (SELECT wordtype_id FROM wordtypes WHERE name='noun'), 'Burning fuel or other material: a cooking fire; a forest fire.', (SELECT lang_id FROM languages WHERE code='en-AU'), (SELECT lang_id FROM languages WHERE code='en-AU')),
+	('fire', (SELECT wordtype_id FROM wordtypes WHERE name='noun'), 'Burning intensity of feeling; ardor.', (SELECT lang_id FROM languages WHERE code='en-AU'), (SELECT lang_id FROM languages WHERE code='en-AU'));
+
+-- add 'flame' tag to all entries with the headword 'fire'
+INSERT INTO
+	entry_tags (tag_id, entry_id)
+SELECT
+  t.tag_id,
+  e.entry_id
+FROM
+  (
+    SELECT
+      tag_id
+    FROM
+      tags
+    WHERE
+      name='flame'
+  ) AS t
+CROSS JOIN
+  (
+    SELECT
+      entry_id
+    FROM
+      entries
+    WHERE
+      headword='fire'
+  ) AS e
+;
